236f43b00f439f13319842c9afe8837b2e7a4183
RahmaYasser <rahma.y9079@gmail.com>
2021-12-29 16:55:05 +0200

    supporting logging to file instead of console

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/hadrianl/ibapi
 go 1.14
 
 require (
+	github.com/natefinch/lumberjack v2.0.0+incompatible
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.16.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
--- utils.go
+++ utils.go
@@ -4,6 +4,10 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/binary"
+	"github.com/natefinch/lumberjack"
+	"go.uber.org/zap/zapcore"
+
+	//"go.uber.org/zap/zapcore"
 	"io"
 	"math"
 	"reflect"
@@ -28,8 +32,30 @@ const (
 
 var log *zap.Logger
 
+
+func getEncoder() zapcore.Encoder {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	return zapcore.NewConsoleEncoder(encoderConfig)
+}
+
+func getLogWriter() zapcore.WriteSyncer {
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   "./test.log",
+		MaxSize:    1,
+		MaxBackups: 5,
+		MaxAge:     30,
+		Compress:   false,
+	}
+	return zapcore.AddSync(lumberJackLogger)
+}
 func init() {
-	log, _ = zap.NewProduction()
+	writeSyncer := getLogWriter()
+	encoder := getEncoder()
+	core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
+	log = zap.New(core, zap.AddCaller())
+	//log, _ = zap.NewProduction()
 }
 
 // APILogger sets the options of internal logger for API, such as level, encoder, output, see uber.org/zap for more information
